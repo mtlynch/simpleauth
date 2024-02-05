b9fd1bbe90d4fea186d13c7b32d472b9b8677d34
Michael Lynch <git@mtlynch.io>
2024-02-04 22:54:34 -0500

    Rename New to NewManager

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -1,11 +1,7 @@
 package sessions
 
 import (
-	"database/sql"
 	"errors"
-
-	"github.com/mtlynch/jeff"
-	jeff_sqlite "github.com/mtlynch/jeff/sqlite"
 )
 
 type (
@@ -17,19 +13,6 @@ type (
 )
 
 var ErrNoSessionFound = errors.New("no active session in request context")
-
-func New(sqliteDB *sql.DB) (Manager, error) {
-	store, err := jeff_sqlite.New(sqliteDB)
-	if err != nil {
-		return Manager{}, err
-	}
-	options := []func(*jeff.Jeff){jeff.CookieName("token")}
-	options = append(options, extraOptions()...)
-	j := jeff.New(store, options...)
-	return Manager{
-		j: j,
-	}, nil
-}
 
 func KeyFromBytes(b []byte) Key {
 	return Key{
