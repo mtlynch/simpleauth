dcfa0c31d23264108e6cce52c9505da57c1f622f
Michael Lynch <git@mtlynch.io>
2024-02-04 22:45:30 -0500

    Delete SessionManager interface
    It's the client who should define the interface.

--- sessions/sessions.go
+++ sessions/sessions.go
@@ -1,26 +1,14 @@
 package sessions
 
 import (
-	"context"
 	"database/sql"
 	"errors"
-	"net/http"
 
 	"github.com/mtlynch/jeff"
 	"github.com/mtlynch/jeff/sqlite"
 )
 
 type (
-	Manager interface {
-		CreateSession(http.ResponseWriter, context.Context, Key, Session) error
-		SessionFromContext(context.Context) (Session, error)
-		EndSession(context.Context, http.ResponseWriter)
-		// WrapRequest wraps the given handler, adding the Session object (if
-		// there's an active session) to the request context before passing control
-		// to the next handler.
-		WrapRequest(http.Handler) http.Handler
-	}
-
 	Key struct {
 		key []byte
 	}
@@ -33,16 +21,16 @@ var ErrNoSessionFound = errors.New("no active session in request context")
 func New(dbPath string) (Manager, error) {
 	db, err := sql.Open("sqlite3", dbPath)
 	if err != nil {
-		return manager{}, err
+		return Manager{}, err
 	}
 	store, err := sqlite.New(db)
 	if err != nil {
-		return manager{}, err
+		return Manager{}, err
 	}
 	options := []func(*jeff.Jeff){jeff.CookieName("token")}
 	options = append(options, extraOptions()...)
 	j := jeff.New(store, options...)
-	return manager{
+	return Manager{
 		j: j,
 	}, nil
 }
